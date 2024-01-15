a5392aad3ce12b03087b3e923988ddf3727443f9
jerlibgnzlz <jerlibgv@gmail.com>
2024-01-15 20:49:53 -0300

    fix  formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Formulario from "../components/Formulario";
 
 
 const NuevoCliente = () => {
@@ -7,11 +8,11 @@ const NuevoCliente = () => {
 
   return (
     <>
-      <h1 className="font-bold text-4xl text-green-700">Nuevo Cliente</h1>
+      <h1 className="font-bold text-4xl text-green-700 ">Nuevo Cliente</h1>
       <p className="mt-3">LLena todos los campos para registrar un nuevo cliente</p>
 
 
-      <div className="flex justify-end">
+      <div className="flex justify-end mt-20">
         <button className="text-white bg-green-500 font-bold px-3 py-1 uppercase rounded-md"
           onClick={() => navigate("/")}
           type="button">volver
@@ -19,7 +20,16 @@ const NuevoCliente = () => {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        <p>formulario aqui</p>
+        <form>
+
+          <Formulario />
+
+          <input
+            type="submit"
+            className="mt-5 w-full bg-green-700 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </form>
       </div>
     </>
   );
