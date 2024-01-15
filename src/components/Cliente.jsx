4630fc4678fcad3cf7ad9afa3cda0a3e2ae708bb
jerlibgnzlz <jerlibgv@gmail.com>
2024-01-15 20:22:54 -0300

    feature botones edit y delete  navigate y formulario

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,12 +1,30 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 
 const Clientes = ({ cliente }) => {
 
-  const { nombre, empresa, email, teñefono, id } = cliente;
+  const { nombre, empresa, email, telefono, id } = cliente;
   return (
-    <tr>
-      <td className="p-6">
-        {nombre}
+    <tr className="border-b">
+      <td className="p-6 space-y-2">
+        <p className="text-2xl text-green-700"> {nombre}</p>
+        <p className=""> {empresa}</p>
       </td>
+
+      <td className="p-6 gap-3">
+        <p className="text-green-700 "><span className="uppercase text-green-800 font-bold">Email: </span>{email}</p>
+        <p className="text-green-700 "><span className="uppercase text-green-800 font-bold">Telefono: </span>{telefono}</p>
+      </td>
+
+      <td className="p-6 flex gap-3 ">
+        <button className="font-bold text-green-600 hover:text-green-700 uppercase text-sm"
+          type="button">Editar</button>
+
+        <button className="font-bold text-red-500 hover:text-red-700 uppercase text-sm"
+          type="button">Eliminar</button>
+      </td>
+
+
     </tr>
   );
 };
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 
 const NuevoCliente = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>Nuevo  Cliente</div>
+    <>
+      <h1 className="font-bold text-4xl text-green-700">Nuevo Cliente</h1>
+      <p className="mt-3">LLena todos los campos para registrar un nuevo cliente</p>
+
+
+      <div className="flex justify-end">
+        <button className="text-white bg-green-500 font-bold px-3 py-1 uppercase rounded-md"
+          onClick={() => navigate("/")}
+          type="button">volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        <p>formulario aqui</p>
+      </div>
+    </>
   );
 };
 
