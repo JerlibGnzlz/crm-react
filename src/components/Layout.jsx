ddc145de0852904211f143608f63b682319646e7
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-01-14 21:01:03 -0300

    feature navegando entre componentes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 const Layout = () => {
   return (
@@ -6,7 +6,15 @@ const Layout = () => {
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-green-900 px-5 py-10">
         <h2 className='font-bold text-4xl text-center text-white'>CRM-Clientes</h2>
+
+        <nav className="mt-10">
+          <NavLink className="text-white block hover:text-yellow-400 mt-4 text-2xl" to="/" >Clientes</NavLink >
+          <NavLink className="text-white block hover:text-yellow-400 mt-4 text-2xl" to="/clientes/nuevo" >Clientes nuevos</NavLink >
+
+        </nav>
       </aside>
+
+
 
       <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
         <Outlet />
