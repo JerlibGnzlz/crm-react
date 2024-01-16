29cb892f94c1ae9cd7e0ea1f7fa86336a3b29214
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-01-16 18:53:37 -0300

    fix validacion del formulario

---
+++ src/components/Error.jsx
@@ -0,0 +1,8 @@
+
+const Error = ({ children }) => {
+  return (
+    <div className="text-center text-black-600 font-bold uppercase p-3 my-4 bg-red-600">{children}</div>
+  );
+};
+
+export default Error;
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,24 +1,48 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useNavigate, Form } from "react-router-dom";
+import { useNavigate, Form, useActionData } from "react-router-dom";
 import Formulario from "../components/Formulario";
+import Error from "../components/Error";
 
 
 
 export async function action({ request }) {
   const formData = await request.formData();
 
-  // const datos = Object.entries(formData);
 
-  console.log([...formData]);
+  const email = formData.get("email");
 
-  return formData;
+  const datos = Object.fromEntries(formData);
+
+  console.log(datos);
+
+
+  const error = [];
+
+  if (Object.values(datos).includes("")) {
+    error.push("Todos los campos son requeridos");
+  }
+
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+
+  if (!regex.test(email)) {
+    error.push("El mail no es valido");
+  }
+
+  if (Object.keys(error).length) {
+    return error;
+  }
+
 }
 
 
 const NuevoCliente = () => {
 
+  const errores = useActionData();
   const navigate = useNavigate();
 
+
+  console.log(errores);
   return (
     <>
       <h1 className="font-bold text-4xl text-green-700 ">Nuevo Cliente</h1>
@@ -33,8 +57,13 @@ const NuevoCliente = () => {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+
+        {errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error>)}
+
+
         <Form
           method="POST"
+          noValidate
         >
           <Formulario />
           <input
